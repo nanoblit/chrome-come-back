342a86d7d18e147afe755aa9877ca98f73831823
nanoblit <jakub.k.maleta@gmail.com>
2020-06-21 23:15:31 +0200

    Add background change and page change detection.

--- background.js
+++ background.js
@@ -1,19 +1,47 @@
-// Copyright 2018 The Chromium Authors. All rights reserved.
-// Use of this source code is governed by a BSD-style license that can be
-// found in the LICENSE file.
+"use strict";
 
-'use strict';
+chrome.runtime.onInstalled.addListener(() => {
+  setColorInStorage("#3aa757");
 
-chrome.runtime.onInstalled.addListener(function() {
-  chrome.storage.sync.set({color: '#3aa757'}, function() {
-    console.log('The color is green.');
+  chrome.tabs.onUpdated.addListener((_, changeInfo, tab) => {
+    printNewPagesUrl(changeInfo, tab);
   });
-  chrome.declarativeContent.onPageChanged.removeRules(undefined, function() {
-    chrome.declarativeContent.onPageChanged.addRules([{
-      conditions: [new chrome.declarativeContent.PageStateMatcher({
-        pageUrl: {hostContains: '.'},
-      })],
-      actions: [new chrome.declarativeContent.ShowPageAction()]
-    }]);
-  });
+
+  InludePageActionOnPagesWithStringInAddress(".");
 });
+
+function setColorInStorage(color) {
+  chrome.storage.sync.set({ color }, () => {
+    console.log("The color is green.");
+  });
+}
+
+function printNewPagesUrl(changeInfo, tab) {
+  chrome.storage.sync.get(["website-address"], (inputs) => {
+    if (!inputs["website-address"] || changeInfo.status !== "complete") {
+      return;
+    }
+    if (stringIsInUrl(inputs["website-address"], tab.url)) {
+      console.log(`New URL: ${tab.url}`);
+    }
+  });
+}
+
+function stringIsInUrl(str, url) {
+  return url.indexOf(str) > -1;
+}
+
+function InludePageActionOnPagesWithStringInAddress(str) {
+  chrome.declarativeContent.onPageChanged.removeRules(undefined, () => {
+    chrome.declarativeContent.onPageChanged.addRules([
+      {
+        conditions: [
+          new chrome.declarativeContent.PageStateMatcher({
+            pageUrl: { hostContains: str },
+          }),
+        ],
+        actions: [new chrome.declarativeContent.ShowPageAction()],
+      },
+    ]);
+  });
+}
--- popup.js
+++ popup.js
@@ -1,21 +1,102 @@
-// Copyright 2018 The Chromium Authors. All rights reserved.
-// Use of this source code is governed by a BSD-style license that can be
-// found in the LICENSE file.
+"use strict";
 
-'use strict';
+const websiteAddressInputElement = document.getElementById("website-address");
+const logoutRedirectAddressInputElement = document.getElementById(
+  "logout-redirect-address"
+);
+const loginPageAddressInputElement = document.getElementById(
+  "login-page-address"
+);
+const usernameFieldSelectorInputElement = document.getElementById(
+  "username-field-selector"
+);
+const passwordFieldSelectorInputElement = document.getElementById(
+  "password-field-selector"
+);
+const signInButtonSelectorInputElement = document.getElementById(
+  "sign-in-button-selector"
+);
+const usernameInputElement = document.getElementById("username");
+const passwordInputElement = document.getElementById("password");
+const activeCheckboxElement = document.getElementById("active");
 
-let changeColor = document.getElementById('changeColor');
+let isActive = false;
 
-chrome.storage.sync.get('color', function(data) {
-  changeColor.style.backgroundColor = data.color;
-  changeColor.setAttribute('value', data.color);
-});
+const elements = [
+  websiteAddressInputElement,
+  logoutRedirectAddressInputElement,
+  loginPageAddressInputElement,
+  usernameFieldSelectorInputElement,
+  passwordFieldSelectorInputElement,
+  signInButtonSelectorInputElement,
+  usernameInputElement,
+  passwordInputElement,
+  activeCheckboxElement,
+];
 
-changeColor.onclick = function(element) {
-  let color = element.target.value;
-  chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
-    chrome.tabs.executeScript(
-        tabs[0].id,
-        {code: 'document.body.style.backgroundColor = "' + color + '";'});
-  });
+// ====================================
+
+for (let element of elements) {
+  setInputInStorageWhenChanged(element);
+}
+
+activeCheckboxElement.onclick = (element) => {
+  const checkbox = element.target;
+  setBackgroundColorsBasedOnCheckbox(checkbox);
 };
+
+// ====================================
+
+function setInputInStorageWhenChanged(element) {
+  element.addEventListener("input", (element) => {
+    const input = element.target;
+    chrome.storage.sync.set(getInputKeyValuePair(input), () => {
+      logNewStorageKeyValuePair(input);
+    });
+  });
+}
+
+function getInputKeyValuePair(input) {
+  return input.type === "checkbox"
+    ? { [input.id]: input.checked }
+    : { [input.id]: input.value };
+}
+
+function logNewStorageKeyValuePair(input) {
+  console.log(
+    `${input.id} is set to ${
+      input.type === "checkbox" ? input.checked : input.value
+    }`
+  );
+}
+
+function setBackgroundColorsBasedOnCheckbox(checkbox) {
+  if (checkbox.checked) {
+    isActive = true;
+    setBackgroundOfEveryPageToColor("green");
+  } else {
+    isActive = false;
+    setBackgroundOfEveryPageToColor("white");
+  }
+}
+
+function setBackgroundOfEveryPageToColor(color) {
+  chrome.tabs.query({}, (tabs) => {
+    for (let tab of tabs) {
+      if (stringIsntInUrl(".", tab.url)) {
+        continue;
+      }
+      setBackgroundOfPageToColor(tab, color)
+    }
+  });
+}
+
+function setBackgroundOfPageToColor(tab, color) {
+  chrome.tabs.executeScript(tab.id, {
+    code: `document.body.style.backgroundColor = "${color}";`,
+  });
+}
+
+function stringIsntInUrl(str, url) {
+  return url.indexOf(str) < 0;
+}
